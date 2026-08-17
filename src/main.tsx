6627ee50ccb2ac8ed49948ca11266beb316f5a34
eddy121384-ui <eddy121384@gmail.com>
2026-08-17 14:06:08 +0800

    debug: route NLSC probe through browser app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AgeMapApp from "./AgeMapApp";
+import NlscLayerProbe from "./NlscLayerProbe";
 import "./styles.css";
+
+const params = new URLSearchParams(window.location.search);
+const showNlscProbe = params.has("nlscProbe");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AgeMapApp />
+    {showNlscProbe ? <NlscLayerProbe /> : <AgeMapApp />}
   </StrictMode>,
 );
